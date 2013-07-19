24724fad5802af14565e18d609ce18d766330832
Rotem Erlich <rotem@rotium.com>
2013-07-19 04:29:40 +0300

    Add Scla-Java Cyclic code

---
+++ MavenScalaJava/JavaScalaMixCode/src/main/java/com/rotium/sj/java/ScalaStackWrapper.java
@@ -0,0 +1,11 @@
+package com.rotium.sj.java;
+
+import com.rotium.sj.scala.SimpleScalaStack;
+
+/**
+ * This is Java class wrapping Scala class to demonstrate Cyclic calls between
+ * Java and Scala.
+ */
+public class ScalaStackWrapper<T> extends SimpleScalaStack<T> {
+
+}
